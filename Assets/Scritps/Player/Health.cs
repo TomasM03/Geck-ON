61c0dce9834a1355982c8168cdb58b09e9d82e84
Tomas Ezequiel Modia <tomasmodia321@gmail.com>
2025-09-15 00:42:36 -0300

    DeadSystem

--- Assets/Scritps/Player/Health.cs
+++ Assets/Scritps/Player/Health.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class Health : MonoBehaviour
 {
-    [Header("Health Settings")]
+    //Sets
     public float maxHealth = 100f;
     public bool isPlayer = false;
 
+    //DeadSets
+    public string mainMenuScene;
+    public float deathDelay = 2f;
+    
+    //Health
     private float currentHealth;
 
     void Start()
@@ -15,22 +22,23 @@ public class Health : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    //DamageSyst
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-
-
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
+    //DiedVoid
     void Die()
     {
         if (isPlayer)
         {
-            Debug.Log("player died");
+            Debug.Log("Player died - Returning to main menu");
+            StartCoroutine(ReturnToMainMenu());
         }
         else
         {
@@ -38,6 +46,33 @@ public class Health : MonoBehaviour
         }
     }
 
+    //BackMenuNUM
+    IEnumerator ReturnToMainMenu()
+    {
+        PlayerController playerController = GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.enabled = false;
+        }
+
+        PlayerCamera playerCam = GetComponentInChildren<PlayerCamera>();
+        if (playerCam != null)
+        {
+            playerCam.DisableCamera();
+        }
+
+        yield return new WaitForSeconds(deathDelay);
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.Disconnect();
+        }
+
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    //Percentage
     public float GetHealthPercentage()
     {
         return currentHealth / maxHealth;
